d6a3e8c5be80ccbbd5a18f9ee15144684c75852a
osarsari <o.sarsari@gmail.com>
2023-05-09 00:23:36 +0200

    redefined the linked list to doubly linked list

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: osarsari <osarsari@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 08:25:41 by osarsari          #+#    #+#             */
-/*   Updated: 2023/05/08 13:22:19 by osarsari         ###   ########.fr       */
+/*   Updated: 2023/05/08 23:28:42 by osarsari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,18 +26,26 @@ typedef enum e_bool{
 	true
 }	t_bool;
 
+typedef struct s_plate
+{
+	struct s_plate	*prev;
+	struct s_plate	*next;
+	int				value;
+}					t_plate;
+
 typedef struct s_stack
 {
-	struct s_stack	*left;
-	struct s_stack	*right;
-	int				value;
-	t_bool			first;
+	struct s_stack	*top;
+	struct s_stack	*bottom;
+	size_t			size;
 }					t_stack;
 
 t_bool	valid_args(int argc, char **argv);
-t_stack	*create_stack(int argc, char **argv);
+t_stack	*pile_stack(int argc, char **argv);
+t_bool	ft_push(t_stack *stack_a, int value);
+t_bool	ft_pop(t_stack *stack_a, int *value);
+t_bool  ft_push_from_to(t_stack *stack_a, t_stack *stack_b);
 void	ft_sort(t_stack *stack_a);
-t_stack	*ft_stacknew(int i);
 t_bool	ft_isspace(int c);
 t_bool	ft_isint(const char *s);
 
