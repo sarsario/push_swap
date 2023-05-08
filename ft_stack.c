3114abbc6c59605d317135f8ad9c9f79aa3670a4
osarsari <o.sarsari@gmail.com>
2023-05-08 13:38:34 +0200

    Poking around creation of the stack

--- ft_stack.c
+++ ft_stack.c
@@ -6,20 +6,30 @@
 /*   By: osarsari <osarsari@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/06 23:33:27 by osarsari          #+#    #+#             */
-/*   Updated: 2023/05/08 12:21:26 by osarsari         ###   ########.fr       */
+/*   Updated: 2023/05/08 13:34:48 by osarsari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-t_stack	*ft_stacknew(int i)
+t_stack	*create_stack(int argc, char **argv)
+{
+	t_stack	*stack_a;
+
+	stack_a = malloc(sizeof(t_stack));
+	if (!stack_a)
+		return (NULL);
+	return (stack_a);
+}
+
+t_stack	*ft_stacknew(int value)
 {
 	t_stack	*stack_new;
 
 	stack_new = malloc(sizeof(t_stack));
 	if (!stack_new)
 		return (NULL);
-	stack_new->value = i;
+	stack_new->value = value;
 	stack_new->left = NULL;
 	stack_new->right = NULL;
 	return (stack_new);
