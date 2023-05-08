59768959f68eebccc341e2116c2cae806b77baf5
osarsari <o.sarsari@gmail.com>
2023-05-09 00:25:08 +0200

    Implemented pile_stack and ft_pop

--- ft_stack.c
+++ ft_stack.c
@@ -6,31 +6,49 @@
 /*   By: osarsari <osarsari@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/06 23:33:27 by osarsari          #+#    #+#             */
-/*   Updated: 2023/05/08 13:34:48 by osarsari         ###   ########.fr       */
+/*   Updated: 2023/05/09 00:15:12 by osarsari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-t_stack	*create_stack(int argc, char **argv)
+t_bool	ft_pop(t_stack *stack_a, int *value)
+{
+	t_plate	*plate_to_remove;
+
+	if (!stack_a->top)
+		return (false);
+	*value = stack_a->top->value;
+	plate_to_remove = stack_a->top;
+	stack_a->top = plate_to_remove->next;
+	if (stack_a->top)
+		stack_a->top->prev = NULL;
+	else
+		stack_a->bottom = NULL;
+	free(plate_to_remove);
+	stack_a->size--;
+	return (true);
+}
+
+t_stack	*pile_stack(int argc, char **argv)
 {
 	t_stack	*stack_a;
+	int		i;
 
 	stack_a = malloc(sizeof(t_stack));
 	if (!stack_a)
 		return (NULL);
+	i = argc;
+	stack_a->size = 0;
+	while (--i > 0)
+	{
+		if (!ft_push(stack_a, ft_atoi(argv[i])))
+		{
+			while (ft_pop(stack_a, &(int){0}))
+				;
+			free(stack_a);
+			return (NULL);
+		}
+	}
 	return (stack_a);
-}
-
-t_stack	*ft_stacknew(int value)
-{
-	t_stack	*stack_new;
-
-	stack_new = malloc(sizeof(t_stack));
-	if (!stack_new)
-		return (NULL);
-	stack_new->value = value;
-	stack_new->left = NULL;
-	stack_new->right = NULL;
-	return (stack_new);
 }
