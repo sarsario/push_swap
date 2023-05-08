94003468192530622c97e0064911bd813452af0f
osarsari <o.sarsari@gmail.com>
2023-05-08 13:25:06 +0200

    create stack before trying to sort

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: osarsari <osarsari@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 08:25:16 by osarsari          #+#    #+#             */
-/*   Updated: 2023/05/08 12:39:00 by osarsari         ###   ########.fr       */
+/*   Updated: 2023/05/08 13:13:09 by osarsari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,19 @@
 
 int	main(int argc, char **argv)
 {
+	stack_t	*stack_a;
+
 	if (argc < 2)
 		return (0);
 	if (!valid_args(argc, argv))
 		ft_putstr_fd("Error\n", 1);
 	else
-		ft_sort(argc, argv);
+	{
+		stack_a = create_stack(argc, argv);
+		if (!stack_a)
+			ft_putstr_fd("Error\n", 1);
+		else
+			ft_sort(stack_a);
+	}
 	return (0);
 }
