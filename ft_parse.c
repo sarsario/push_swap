cee3573ef9749f3eece9cce5fbf540294c5c8102
osarsari <o.sarsari@gmail.com>
2023-05-08 12:55:28 +0200

    parsing argv

--- ft_parse.c
+++ ft_parse.c
@@ -6,7 +6,7 @@
 /*   By: osarsari <osarsari@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 10:00:29 by osarsari          #+#    #+#             */
-/*   Updated: 2023/05/08 12:13:58 by osarsari         ###   ########.fr       */
+/*   Updated: 2023/05/08 12:37:58 by osarsari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,19 +21,32 @@ t_bool	ft_isspace(int c)
 	return (false);
 }
 
-t_bool	valid_stack(int argc, char **argv)
+static t_bool	unique_atoi(char **argv, int i)
+{
+	int	j;
+
+	if (i < 2)
+		return (true);
+	j = i;
+	while (--j > 0)
+	{
+		if (ft_atoi(argv[j] == ft_atoi(argv[i])))
+			return (false);
+	}
+	return (true);
+}
+
+t_bool	valid_args(int argc, char **argv)
 {
 	int		i;
-	t_stack	*stack_a;
 
-	i = 1;
-	if (!ft_isint(argv[i]))
-		return (false);
-	stack_a = ft_stacknew(ft_atoi(argv[i]));
+	i = 0;
 	while (++i < argc)
 	{
 		if (!ft_isint(argv[i]))
 			return (false);
-		
+		if (!unique_atoi(argv, i))
+			return (false);
 	}
+	return (true);
 }
