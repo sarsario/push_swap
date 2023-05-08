1304746a925fd93075dfd381976a460042da5de1
osarsari <o.sarsari@gmail.com>
2023-05-08 12:56:13 +0200

    renamed to valid_args

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: osarsari <osarsari@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 08:25:16 by osarsari          #+#    #+#             */
-/*   Updated: 2023/05/08 11:45:21 by osarsari         ###   ########.fr       */
+/*   Updated: 2023/05/08 12:39:00 by osarsari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ int	main(int argc, char **argv)
 {
 	if (argc < 2)
 		return (0);
-	if (!valid_stack(argc, argv))
-		ft_putstr("Error\n");
+	if (!valid_args(argc, argv))
+		ft_putstr_fd("Error\n", 1);
 	else
 		ft_sort(argc, argv);
 	return (0);
