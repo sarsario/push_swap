2c54cab633b71e12897dcd3afc998b9903e098c3
osarsari <o.sarsari@gmail.com>
2023-05-09 12:45:13 +0200

    ft_move redone. Added documentation

--- ft_push.c
+++ ft_push.c
@@ -6,13 +6,24 @@
 /*   By: osarsari <osarsari@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/06 18:28:52 by osarsari          #+#    #+#             */
-/*   Updated: 2023/05/08 23:53:01 by osarsari         ###   ########.fr       */
+/*   Updated: 2023/05/09 12:27:25 by osarsari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-t_bool	ft_push(t_stack *stack_a, int value)
+/*
+** Pushes a new plate with the given value onto the top of the stack.
+**
+** stack: The stack to push the new plate onto.
+** value: The value to be assigned to the new plate.
+**
+** Returns:
+** true if the push was successful, false otherwise (in case of
+** memory allocation failure).
+*/
+
+t_bool	ft_push(t_stack *stack, int value)
 {
 	t_plate	*new_plate;
 
@@ -21,26 +32,43 @@ t_bool	ft_push(t_stack *stack_a, int value)
 		return (false);
 	new_plate->value = value;
 	new_plate->prev = NULL;
-	new_plate->next = stack_a->top;
-	if (stack_a->top)
-		stack_a->top->prev = new_plate;
+	new_plate->next = stack->top;
+	if (stack->top)
+		stack->top->prev = new_plate;
 	else
-		stack_a->bottom = new_plate;
-	stack_a->top = new_plate;
-	stack_a->size++;
+		stack->bottom = new_plate;
+	stack->top = new_plate;
+	stack->size++;
 	return (true);
 }
 
-t_bool	ft_push_from_to(t_stack *stack_a, t_stack *stack_b)
-{
-	int	value;
+/*
+** Move the top plate of a source stack to the top of a destination stack.
+**
+** src: The source stack.
+** dst: The destination stack.
+**
+** Returns:
+** true if the move was successful, false otherwise.
+*/
 
-	if (!stack_a->bottom)
-		return(true);
-	value = 0;
-	if (!ft_pop(stack_a, &value))
+t_bool	ft_move(t_stack *src, t_stack *dst)
+{
+	t_plate	*top;
+
+	if (src->size < 1)
 		return (false);
-	if (!ft_push(stack_b, value))
-		return (false);
+	top = src->top;
+	src->top = top->next;
+	src->top->prev = NULL;
+	src->size--;
+	top->next = dst->top;
+	top->prev = NULL;
+	if (dst->size == 0)
+		dst->bottom = top;
+	else
+		dst->top->prev = top;
+	dst->top = top;
+	dst->size++;
 	return (true);
 }
