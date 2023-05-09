0746edf6e248ee352f5b693de3e13f5496950399
osarsari <o.sarsari@gmail.com>
2023-05-09 12:45:57 +0200

    ft_swap implemented and documented

--- ft_swap.c
+++ ft_swap.c
@@ -1,13 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   swap.c                                             :+:      :+:    :+:   */
+/*   ft_swap.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: osarsari <osarsari@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/06 18:30:13 by osarsari          #+#    #+#             */
-/*   Updated: 2023/05/06 18:30:21 by osarsari         ###   ########.fr       */
+/*   Updated: 2023/05/09 12:39:26 by osarsari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+/*
+** Swap the two top plates of the stack.
+**
+** stack: The stack to swap the plates.
+**
+** Returns:
+** true if the swap was successful, false otherwise.
+*/
+
+t_bool	ft_swap(t_stack *stack)
+{
+	if (stack->size < 2)
+		return (false);
+	stack->top->value ^= stack->top->next->value;
+	stack->top->next->value ^= stack->top->value;
+	stack->top->value ^= stack->top->next->value;
+	return (true);
+}
