e96803eafc6a65d7df16318d7d04823d09c1192e
osarsari <o.sarsari@gmail.com>
2023-05-10 22:52:03 +0200

    Norminette fix

--- ft_print_stack.c
+++ ft_print_stack.c
@@ -6,7 +6,7 @@
 /*   By: osarsari <osarsari@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 20:24:39 by osarsari          #+#    #+#             */
-/*   Updated: 2023/05/10 21:15:23 by osarsari         ###   ########.fr       */
+/*   Updated: 2023/05/10 22:51:50 by osarsari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	ft_print_stack(t_stack *stack)
 	t_plate	*current;
 
 	if (!stack)
-		return ft_putstr_fd("stack doesn't exist\n", 1);
+		return (ft_putstr_fd("stack doesn't exist\n", 1));
 	if (stack->size < 1)
 		return (ft_putstr_fd("Empty stack.\n", 1));
 	current = stack->top;
