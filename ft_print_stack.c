3b03af6852df26ecfcf4581a1596d8b04ab3f107
osarsari <o.sarsari@gmail.com>
2023-05-10 22:46:36 +0200

    ft_print_stack implemented

---
+++ ft_print_stack.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_stack.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: osarsari <osarsari@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/09 20:24:39 by osarsari          #+#    #+#             */
+/*   Updated: 2023/05/10 21:15:23 by osarsari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	ft_print_stack(t_stack *stack)
+{
+	t_plate	*current;
+
+	if (!stack)
+		return ft_putstr_fd("stack doesn't exist\n", 1);
+	if (stack->size < 1)
+		return (ft_putstr_fd("Empty stack.\n", 1));
+	current = stack->top;
+	while (current)
+	{
+		ft_printf("%i\n", current->value);
+		current = current->next;
+	}
+}
