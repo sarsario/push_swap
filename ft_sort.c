902c748e023bde266c7355839aa844af03dc83a4
osarsari <o.sarsari@gmail.com>
2023-05-11 00:59:39 +0200

    stack_is_sorted implemented and documented

--- ft_sort.c
+++ ft_sort.c
@@ -6,11 +6,38 @@
 /*   By: osarsari <osarsari@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 11:47:23 by osarsari          #+#    #+#             */
-/*   Updated: 2023/05/11 00:12:51 by osarsari         ###   ########.fr       */
+/*   Updated: 2023/05/11 00:59:14 by osarsari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+/*
+** Checks whether the plates in the given stack are sorted in ascending order.
+**
+** stack: The stack to check.
+**
+** Returns:
+** true if the plates are sorted, false otherwise.
+*/
+
+t_bool	stack_is_sorted(t_stack *stack)
+{
+	t_plate	*current;
+
+	if (!stack)
+		return (false);
+	if (stack->size < 2)
+		return (true);
+	current = stack->top->next;
+	while (current)
+	{
+		if (current->prev->value > current->value)
+			return (false);
+		current = current->next;
+	}
+	return (true);
+}
 
 void	ft_sort(t_stack *stack_a, t_stack *stack_b)
 {
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: osarsari <osarsari@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 08:25:41 by osarsari          #+#    #+#             */
-/*   Updated: 2023/05/10 22:53:10 by osarsari         ###   ########.fr       */
+/*   Updated: 2023/05/11 00:25:52 by osarsari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,6 +53,7 @@ t_bool	ft_swap(t_stack *stack);
 t_bool	ft_rotate(t_stack *stack);
 t_bool	ft_reverse_rotate(t_stack *stack);
 void	ft_print_stack(t_stack *stack);
+t_bool	stack_is_sorted(t_stack *stack);
 void	ft_sort(t_stack *stack_a, t_stack *stack_b);
 
 #endif
