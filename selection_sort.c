c66cee821b72bb6a59f6328ab1653ba6c9f636bc
Oussama Sarsari <osarsari@student.s19.be>
2023-05-18 18:51:58 +0200

    selection_sort need fixing condition

--- selection_sort.c
+++ selection_sort.c
@@ -6,28 +6,76 @@
 /*   By: osarsari <osarsari@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 18:55:12 by osarsari          #+#    #+#             */
-/*   Updated: 2023/05/18 15:58:10 by osarsari         ###   ########.fr       */
+/*   Updated: 2023/05/18 18:51:41 by osarsari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	push_phase(t_stack *stack_a, t_stack *stack_b,
-	t_plate *smallest, t_plate *biggest)
+void	push_beginning(t_stack *stack_a, t_stack *stack_b, int pivot)
 {
-	if (!stack_a || !stack_b || !smallest || !biggest)
+	if (!stack_a || !stack_b)
 		return ;
-	return ;
+	if (stack_a->top->value < pivot)
+	{
+		if (stack_a->top->next->value < stack_a->top->value)
+		{
+			if (stack_a->top->next->value < stack_a->bottom->value)
+			{
+				if (stack_b->top->next->value > stack_b->top->value)
+					ft_ss(stack_a, stack_b);
+				else
+					ft_sa(stack_a);
+			}
+		}
+		else if (stack_a->bottom->value < stack_a->top->value)
+		{
+			if (stack_b->bottom->value > stack_b->top->value)
+				ft_rrr(stack_a, stack_b, 1);
+			else
+				ft_rra(stack_a, 1);
+		}
+		ft_pb(stack_a, stack_b);
+	}
+	else if (stack_a->bottom->value < stack_a->top->value)
+	{
+		if (stack_b->bottom->value > stack_b->top->value)
+		{
+			ft_rrr(stack_a, stack_b, 1);
+			ft_ss(stack_a, stack_b);
+		}
+		else
+		{
+			ft_rra(stack_a, 1);
+			ft_sa(stack_a);
+		}
+	}
+	// else if (stack_a->top->value < stack_a->top->next->value)
+	// {
+	// 	if (stack_b->top->next->value > stack_b->top->value)
+	// 		ft_ss(stack_a, stack_b);
+	// 	else
+	// 		ft_sa(stack_a);
+	// }
+	// else if ()
+	// else
+	// 	ft_ra(stack_a, 1);
+	ft_printf("\nStack_a:\n");
+	ft_print_stack(stack_a);
+	ft_printf("\nStack_b:\n");
+	ft_print_stack(stack_b);
+	ft_printf("=======\n");
+	sleep(1);
 }
 
 void	selection_sort(t_stack *stack_a, t_stack *stack_b)
 {
-	t_plate	*smallest;
-	t_plate	*biggest;
+	int	nb_plates;
 
 	if (!stack_a || !stack_b)
 		return (ft_putstr_fd("Error\n", 1));
-	smallest = get_smallest_plate(stack_a);
-	biggest = get_biggest_plate(stack_a);
-	push_phase(stack_a, stack_b, smallest, biggest);
+	nb_plates = stack_a->size + stack_b->size;
+	ft_printf("stack_a->size = %i\n", stack_a->size);
+	while (stack_a->size > stack_b->size)
+		push_beginning(stack_a, stack_b, nb_plates / 2);
 }
