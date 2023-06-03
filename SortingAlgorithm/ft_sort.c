316001a41e9f415a87918d76e6d347533adc3a6a
Oussama Sarsari <osarsari@student.s19.be>
2023-06-03 12:30:11 +0200

    So close, yet so far

--- SortingAlgorithm/ft_sort.c
+++ SortingAlgorithm/ft_sort.c
@@ -6,7 +6,7 @@
 /*   By: osarsari <osarsari@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 14:57:27 by osarsari          #+#    #+#             */
-/*   Updated: 2023/06/03 12:23:25 by osarsari         ###   ########.fr       */
+/*   Updated: 2023/06/03 12:28:43 by osarsari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,10 +70,13 @@ void	quick_sort(t_stack *stack_a, t_stack *stack_b)
 {
 	if (stack_is_asc(stack_a) && stack_b->size == 0)
 		return ;
-	if (stack_is_asc(stack_a) && !stack_is_dsc(stack_b))
-		top_half_pa(stack_a, stack_b);
-	else if (stack_is_asc(stack_a))
+	if (stack_is_asc(stack_a) && stack_is_dsc(stack_b))
+	{
 		ft_pa(stack_a, stack_b);
+		return (quick_sort(stack_a, stack_b));
+	}
+	if (stack_is_asc(stack_a))
+		top_half_pa(stack_a, stack_b);
 	if (!stack_is_asc(stack_a) && stack_a->size > 3)
 		lower_half_pb(stack_a, stack_b);
 	if (!stack_is_asc(stack_a) && stack_a->size < 4)
